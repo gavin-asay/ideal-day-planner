fff664591a994431dee75640e82b260c37558611
Gavin Asay <gavin.asay@gmail.com>
2020-12-02 05:18:01 -0700

    planner layout

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,21 @@
 $("#currentDay").text(dayjs().format("dddd, MMMM DD, YYYY"));
+$("#currentTime").text(dayjs().format("hh:mm:ss a"));
+
+setInterval(function() {
+    $("#currentTime").text(dayjs().format("hh:mm:ss a"));
+}, 10);
+
+// let hour = 12;
+let hour = dayjs().hour();
+let scheduleList = $(".row");
+
+scheduleList.each(function() {
+    if (parseInt(this.id) < hour) {
+        $(this).children(".col-8").addClass("past");
+    } else if (parseInt(this.id) === hour) {
+        $(this).children(".col-8").addClass("present");
+    } else if (parseInt(this.id) > hour) {
+        $(this).children(".col-8").addClass("future");
+    }
+});
+
