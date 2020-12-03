22a378787d08ecfb2d39b3ede3347cff94dc8310
Gavin Asay <gavin.asay@gmail.com>
2020-12-03 06:10:51 -0700

    tinkering with jQuery

--- Develop/script.js
+++ Develop/script.js
@@ -3,19 +3,51 @@ $("#currentTime").text(dayjs().format("hh:mm:ss a"));
 
 setInterval(function() {
     $("#currentTime").text(dayjs().format("hh:mm:ss a"));
-}, 10);
+}, 1000);
 
 // let hour = 12;
+let savedTasks = {};
 let hour = dayjs().hour();
 let scheduleList = $(".row");
+// scheduleList = jQuery.makeArray(scheduleList);
+let scheduleText = $(".schedule-text").text();
 
-scheduleList.each(function() {
+let highlight = function() {
     if (parseInt(this.id) < hour) {
-        $(this).children(".col-8").addClass("past");
+        $(this).children(".col-8").removeClass("past present future").addClass("past");
     } else if (parseInt(this.id) === hour) {
-        $(this).children(".col-8").addClass("present");
+        $(this).children(".col-8").removeClass("past present future").addClass("present");
     } else if (parseInt(this.id) > hour) {
-        $(this).children(".col-8").addClass("future");
+        $(this).children(".col-8").removeClass("past present future").addClass("future");
     }
-});
+}
 
+scheduleList.each(highlight);
+
+setInterval(function() {
+    scheduleList.each(highlight);
+}, 1000 * 60 * 10);
+
+let taskSave = function() {
+    savedTasks = {
+        8: $("#8 > .col-8").text(),
+        9: $("#9 > .col-8").text(),
+        10: $("#10 > .col-8").text(),
+        11: $("#11 > .col-8").text(),
+        12: $("#12 > .col-8").text(),
+        13: $("#13 > .col-8").text(),
+        14: $("#14 > .col-8").text(),
+        15: $("#15 > .col-8").text(),
+        16: $("#16 > .col-8").text(),
+        17: $("#17 > .col-8").text(),
+        18: $("#18 > .col-8").text()
+    };
+ 
+    localStorage.setItem("tasks", JSON.stringify(savedTasks));
+}
+
+let taskLoad = function() {
+    savedTasks = JSON.parse(localStorage.getItem("tasks"));
+}
+
+// taskSave();
